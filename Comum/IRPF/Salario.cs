ab80471e88f7e166a1c37e2abaf5d87bae3a7d8f
Júlio César Oliveira Rodrigues <senhorodrigues@live.com>
2021-11-05 10:03:39 -0300

    Criação da tabela de imposto

--- Comum/IRPF/Salario.cs
+++ Comum/IRPF/Salario.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 
 namespace Maths.Comum.IRPF {
+    public static class Tabela2021 {
+        public static (double salario, float percentual)                               PrimeiraFaixaSalario = (1903.99, 7.5f);
+        public static (double salario, float percentual, double descontoFaixaAnterior) SegundaFaixaSalario  = (2826.66, 15f, 69.20);
+        public static (double salario, float percentual, double descontoFaixaAnterior) TerceiraFaixaSalario = (3751.06, 22.5f, 207.86);
+        public static (double salario, float percentual, double descontoFaixaAnterior) QuartaFaixaSalario   = (4664.69, 27.5f, 413.42);
+    }
     public static class Salario {
         public static double CalcularIRPF(int AnoVigencia, double Salario = 0, double QuantidadeDependentes = 0) {
             double retorno;
@@ -9,10 +15,10 @@ namespace Maths.Comum.IRPF {
             Salario -= INSS.Salario.CalcularINSS(2020, Salario);
 
             retorno = Salario switch {
-                _ when Salario >= 1903.99 && Salario <= 2826.65 => ((Salario - 1903.99) * 7.5 / 100) - (QuantidadeDependentes * parcelaPorDepente),
-                _ when Salario >= 2826.66 && Salario <= 3751.05 => ((Salario - 2826.66) * 15 / 100) - (QuantidadeDependentes * parcelaPorDepente) + 69.20,
-                _ when Salario >= 3751.06 && Salario <= 4664.68 => ((Salario - 3751.06) * 22.5 / 100) - (QuantidadeDependentes * parcelaPorDepente) + 207.86,
-                _ when Salario >= 4664.69  => ((Salario - 4664.69) * 27.5 / 100) - (QuantidadeDependentes * parcelaPorDepente) + 413.42,
+                _ when Salario >= Tabela2021.PrimeiraFaixaSalario.salario && Salario < Tabela2021.SegundaFaixaSalario.salario => ((Salario - Tabela2021.PrimeiraFaixaSalario.salario) * Tabela2021.PrimeiraFaixaSalario.percentual / 100) - (QuantidadeDependentes * parcelaPorDepente),
+                _ when Salario >= Tabela2021.SegundaFaixaSalario.salario && Salario < Tabela2021.TerceiraFaixaSalario.salario => ((Salario - Tabela2021.SegundaFaixaSalario.salario) * Tabela2021.SegundaFaixaSalario.percentual / 100) - (QuantidadeDependentes * parcelaPorDepente) + Tabela2021.SegundaFaixaSalario.descontoFaixaAnterior,
+                _ when Salario >= Tabela2021.TerceiraFaixaSalario.salario && Salario < Tabela2021.QuartaFaixaSalario.salario => ((Salario - Tabela2021.TerceiraFaixaSalario.salario) * Tabela2021.TerceiraFaixaSalario.percentual / 100) - (QuantidadeDependentes * parcelaPorDepente) + Tabela2021.TerceiraFaixaSalario.descontoFaixaAnterior,
+                _ when Salario >= Tabela2021.QuartaFaixaSalario.salario => ((Salario - Tabela2021.QuartaFaixaSalario.salario) * Tabela2021.QuartaFaixaSalario.percentual / 100) - (QuantidadeDependentes * parcelaPorDepente) + Tabela2021.QuartaFaixaSalario.descontoFaixaAnterior,
                 _ => 0
             };
 
